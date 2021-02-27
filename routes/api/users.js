6ea685b869c9a59f4d69bf7b04a505a4dc035014
Joshua Favre <joshuaf@MacBook-Pro.local>
2021-02-26 23:00:01 -0600

    add jwtSecret to config, and jwt in users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { check, validationResult } = require('express-validator/check');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
 
@@ -32,7 +33,9 @@ router.post(
 			let user = await User.findOne({ email });
 
 			if (user) {
-				return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+				return res
+					.status(400)
+					.json({ errors: [{ msg: 'User already exists' }] });
 			}
 
 			const avatar = gravatar.url(email, {
@@ -54,14 +57,25 @@ router.post(
 
 			await user.save();
 
-			// Return jsonwebtoken
-			res.send('User registered');
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
+
+			jwt.sign(
+				payload,
+				config.get('jwtSecret'),
+				{ expiresIn: 360000 },
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
 		} catch (err) {
 			console.log(err.message);
 			res.status(500).send('Server error');
 		}
-
-		res.send('User route');
 	}
 );
 
