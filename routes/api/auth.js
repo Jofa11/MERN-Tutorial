bcce3e57fb4970e9e583cc7a5a0036fa6b8fc532
Joshua Favre <joshuaf@MacBook-Pro.local>
2021-02-26 23:12:29 -0600

    create auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-// @route   GET qpi/auth
+// @route   GET api/auth
 // @desc    Test route
 // @access  Public
 router.get('/', (req, res) => res.send('Auth route'));
