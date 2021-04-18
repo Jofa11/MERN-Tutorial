7b10052fd3f81b2f0a6fd15d88e696af0c4e7704
Joshua <favrejoshua@yahoo.com>
2021-04-17 23:51:57 -0500

    create profile item component

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -12,21 +12,24 @@ const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
 
 	return (
 		<Fragment>
-			{loading ? (
+			{ loading ? (
 				<Spinner />
 			) : (
 				<Fragment>
 					<h1 className='large text-primary'>Developers</h1>
 					<p className='lead'>
-						<i className='fab fa-connectdevelop'></i> Browse and connect with developers
+						<i className='fab fa-connectdevelop'></i> Browse and connect with
+						developers
 					</p>
-                    <div className='profiles'>
-                        {profiles.length > 0 ? (
-                            profiles.map(profile => (
-                                <ProfileItem key={profile._id} profile={profile} />
-                            ))
-                        ) : <h4>No profiles found...</h4>}
-                    </div>
+					<div className='profiles'>
+						{profiles.length > 0 ? (
+							profiles.map((profile) => (
+								<ProfileItem key={profile._id} profile={profile} />
+							))
+						) : (
+							<h4>No profiles found...</h4>
+						)}
+					</div>
 				</Fragment>
 			)}
 		</Fragment>
